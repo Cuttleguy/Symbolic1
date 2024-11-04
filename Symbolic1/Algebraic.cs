5fd38c705519d22acf21cc91ba1a40928c431e38
Cuttleguy <123271904+Cuttleguy@users.noreply.github.com>
2024-11-04 16:37:27 -0500

    11/4 at school

---
+++ Symbolic1/Algebraic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Symbolic1
+{
+    public class Algebraic<T> 
+    {
+        public T constant;
+        public List<Radical<T>> radicals;
+    }
+}
---
+++ Symbolic1/Radical.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Symbolic1
+{
+    public class Radical<T> :
+    IMultiplyOperators<Radical<T>, Radical<T>, Radical<T>>,
+        IDivisionOperators<Radical<T>, Radical<T>, Radical<T>>,
+
+    {
+        public static Radical<T> operator *(Radical<T> left, Radical<T> right)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
